c225a9644c04b6478b7a7cf9ae73d7572e3ebe24
DonCuervoC <nelson.cuervo89@gmail.com>
2023-04-19 09:18:58 -0400

    get user data

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,19 @@
+const User = require("../models/user")
+
 async function getMe(req, res) {
 
-    res.status(200).send({ msg: "OK" });
+const{user_id} = req.user;
+
+const response = await User.findById(user_id);
+
+if(!response){
+    res.status(400).send({msg: " Not user found"});
+} else{
+    res.status(200).send(response);
+}
+
+    // console.log(req.user);
+   // res.status(200).send({ msg: "OK" });
     
 
 }
