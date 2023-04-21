4b4edc4bed6412bbfaefe0212116bae48e4b69fc
DonCuervoC <nelson.cuervo89@gmail.com>
2023-04-21 15:00:06 -0400

    BackEnd Courses deleting OK

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -1,4 +1,4 @@
-const course = require("../models/course");
+
 const Course = require("../models/course");
 const image = require("../utils/image");
 
@@ -19,14 +19,14 @@ async function createCourse(req, res) {
 async function getCourses(req, res) {
 
     // /courses?page=2&limit=1
-    const { page = 1, limit = 10} = req.query;
+    const { page = 1, limit = 10 } = req.query;
 
     const options = {
         page: parseInt(page),
         limit: parseInt(limit)
     };
 
-    Course.paginate({}, options, (error, courses) =>{
+    Course.paginate({}, options, (error, courses) => {
 
         if (error) {
             res.status(400).send({ msg: "Error while getting courses" })
@@ -35,19 +35,53 @@ async function getCourses(req, res) {
         }
     })
 
+    /*
+        try {
+        const courses = await Course.find();
+        res.status(200).send(courses);
+    } catch (error) {
+        res.status(400).send({ msg: "Error while getting courses" });
+    }
+*/
 }
 
+async function updateCourse(req, res) {
 
-    // try {
-    //     const courses = await Course.find();
-    //     res.status(200).send(courses);
-    // } catch (error) {
-    //     res.status(400).send({ msg: "Error while getting courses" });
-    // }
+    const { id } = req.params;
+    const courseData = req.body;
+
+    if (req.files.miniature) {
+        const imagePath = image.getFilePath(req.files.miniature);
+        courseData.miniature = imagePath;
+    }
+
+    try {
+        await Course.findByIdAndUpdate({ _id: id }, courseData);
+        res.status(200).send({ msg: "Course updated" });
+    } catch (error) {
+        res.status(400).send({ msg: "Error while updating course" });
+    }
+
+}
+
+async function deleteCourse(req, res) {
+
+    const { id } = req.params;
+
+    try {
+        await Course.findByIdAndDelete({ _id: id });
+        res.status(200).send({ msg: "Course deleted" });
+    } catch (error) {
+        res.status(400).send({ msg: "Error while deleting course" });
+    }
+}
+
 
 module.exports = {
 
     createCourse,
     getCourses,
+    updateCourse,
+    deleteCourse,
 
 };
--- server/router/course.js
+++ server/router/course.js
@@ -9,6 +9,8 @@ const api = express.Router();
 //APIs...
 api.post("/course", [md_auth.asureAuth, md_upload], CourseController.createCourse);
 api.get("/courses", CourseController.getCourses);
+api.patch("/course/:id", [md_auth.asureAuth, md_upload], CourseController.updateCourse);
+api.delete("/course/:id", [md_auth.asureAuth], CourseController.deleteCourse);
 
 
 module.exports = api;
