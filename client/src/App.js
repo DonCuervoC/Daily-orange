a603ad0f09627df681f24ae044d8dc6e6f5c263c
DonCuervoC <nelson.cuervo89@gmail.com>
2023-04-24 20:30:07 -0400

    FrontEnd : SASS & Semantic UI OK

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 export default function App() {
   return (
@@ -6,6 +7,10 @@ export default function App() {
       <h1>
         We're on my Web
       </h1>
+      <div>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
     </div>
   )
 }
