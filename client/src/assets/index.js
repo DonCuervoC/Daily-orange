6180af7aff330e40cedcc670184a4f1a483368e5
DonCuervoC <nelson.cuervo89@gmail.com>
2023-04-25 14:25:46 -0400

    FrontEnd : update 1 images

---
+++ client/src/assets/index.js
@@ -0,0 +1,21 @@
+import { ReactComponent as logOrange } from "./svg/orange-news.svg";
+import { ReactComponent as LogoWithe } from "./svg/tincode-white.svg";
+import authBg from "./jpg/auth-bg.jpg";
+import homeBanner from "./jpg/home-banner.jpg";
+import noAvatar from "./jpg/no-avatar.jpg";
+import academyLogo from "./png/academy-logo.png";
+
+const Icon = {
+    logOrange,
+    LogoWithe,
+};
+
+const Images = {
+    logOrange,
+    authBg,
+    homeBanner,
+    noAvatar,
+    academyLogo,
+};
+
+export { Icon, Images };
