9997e38b477819bc7d0483441279d2b2d2008be9
DonCuervoC <nelson.cuervo89@gmail.com>
2023-04-27 15:12:02 -0400

    FrontEnd: Register user OK

--- client/src/components/Admin/Auth/RegisterForm/RegisterForm.form.js
+++ client/src/components/Admin/Auth/RegisterForm/RegisterForm.form.js
@@ -1,3 +1,5 @@
+import * as Yup from "yup";
+
 export function initialValues() {
 
     return {
@@ -6,4 +8,14 @@ export function initialValues() {
         confirmtPassword: "",
         conditionsAccepted: false,
     };
+}
+
+export function validationSchema(){
+
+    return Yup.object({
+        email: Yup.string().email("Email not valid").required("Email is an obligatory field"),
+        password: Yup.string().required("Password is an obligatory field"),
+        confirmtPassword: Yup.string().required("Password is an obligatory field").oneOf([Yup.ref("password")], "Passwords must match"),
+        conditionsAccepted: Yup.bool().isTrue(true),
+    });
 }
--- client/src/components/Admin/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Admin/Auth/RegisterForm/RegisterForm.js
@@ -1,42 +1,57 @@
 import React, { useState } from 'react';
 import { Form } from 'semantic-ui-react';
 import { useFormik } from 'formik';
-import { initialValues } from "./RegisterForm.form";
+import { Auth } from '../../../../api';
+import { initialValues, validationSchema } from "./RegisterForm.form";
 import "./RegisterForm.scss";
 
 
-export function RegisterForm() {
+const authController = new Auth();
 
+export function RegisterForm(props) {
+
+    const {openLogin} = props;
     const [error, setError] = useState("");
 
     const formik = useFormik({
 
         initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+
         onSubmit: async (formValue) => {
             try {
+                setError("");
                 console.log(formValue);
+                await authController.register(formValue);
+                openLogin();
+
             } catch (error) {
                 console.log(error);
+                setError("Error on server");
             }
-
-        }
-    })
+        },
+    });
 
     return (
         <div>
             <Form className='register-form' onSubmit={formik.handleSubmit} >
                 <Form.Input name="email" placeholder="E-mail"
                     onChange={formik.handleChange} value={formik.values.email}
+                    error={formik.errors.email}
                 />
                 <Form.Input name="password" type='password' placeholder="Password"
                     onChange={formik.handleChange} value={formik.values.password}
+                    error={formik.errors.password}
                 />
                 <Form.Input name="confirmtPassword" type='password' placeholder="Confirm Password"
                     onChange={formik.handleChange} value={formik.values.confirmtPassword}
+                    error={formik.errors.confirmtPassword}
                 />
                 <Form.Checkbox name="conditionsAccepted" label="I have read and accept the privacy policy"
                     onChange={(_, data) => formik.setFieldValue("conditionsAccepted", data.checked)}
                     checked={formik.values.conditionsAccepted}
+                    error={formik.errors.conditionsAccepted}
                 />
                 <Form.Button type='submit' primary fluid loading={formik.isSubmitting}>
                     Create Account
