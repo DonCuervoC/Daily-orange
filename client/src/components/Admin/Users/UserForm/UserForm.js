1eb8e19e849a1d65a768df0d4842158260b28df3
DonCuervoC <nelson.cuervo89@gmail.com>
2023-05-02 10:52:44 -0400

    FrontEnd: control and validating form OK

--- client/src/components/Admin/Users/UserForm/UserForm.js
+++ client/src/components/Admin/Users/UserForm/UserForm.js
@@ -1,28 +1,54 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./UserForm.form";
+import "./UserForm.scss";
 
 export function UserForm(props) {
     const { close, onReload, user } = props;
 
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+        onSubmit: async (formValue) => {
+            try {
+                console.log(formValue);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })
+
     return (
-        <Form className='user-form'>
-           <div className='user-form__avatar'>
-            <span>AVATAR</span>
-           </div>
+        <Form className='user-form' onSubmit={formik.handleSubmit} >
+            <div className='user-form__avatar'>
+                <span>AVATAR</span>
+            </div>
 
-           <Form.Group widths="equal">
-                <Form.Input  name="firstname" placeholder="First Name" />
-                <Form.Input  name="lastname" placeholder="Last Name" />
-           </Form.Group>
+            <Form.Group widths="equal">
+                <Form.Input name="firstname" placeholder="First Name"
+                    onChange={formik.handleChange} value={formik.values.firstname} error={formik.errors.firstname} />
+                <Form.Input name="lastname" placeholder="Last Name"
+                    onChange={formik.handleChange} value={formik.values.lastname} error={formik.errors.lastname} />
+            </Form.Group>
 
-           <Form.Group widths="equal">
-                <Form.Input  name="email" placeholder="E-mail" />
-                <Form.Dropdown  placeholder='Select a role' options={roleOptions} selection />
-           </Form.Group>
+            <Form.Group widths="equal">
+                <Form.Input name="email" placeholder="E-mail"
+                    onChange={formik.handleChange} value={formik.values.email} error={formik.errors.email} />
+                <Form.Dropdown placeholder='Select a role'
+                    options={roleOptions}
+                    selection
+                    onChange={(__, data) => formik.setFieldValue("role", data.value)}
+                    value={formik.values.role}
+                    error={formik.errors.role}
+                />
+            </Form.Group>
 
-           <Form.Input  type='password' name="password" placeholder="Password"/>
+            <Form.Input type='password' name="password" placeholder="Password"
+                onChange={formik.handleChange} value={formik.values.password} error={formik.errors.password} />
 
-            <Form.Button type='submit' primary fluid>
+            <Form.Button type='submit' primary fluid loading={formik.isSubmitting}>
                 {user ? "Update user" : "Create user"}
             </Form.Button>
 
@@ -30,16 +56,15 @@ export function UserForm(props) {
     );
 }
 
-
 const roleOptions = [
     {
         key: "user",
-        text:"User",
-        values: "user",
+        text: "User",
+        value: "user",
     },
     {
         key: "admin",
-        text:"Administrator",
-        values: "admin",
+        text: "Administrator",
+        value: "admin",
     },
 ]
