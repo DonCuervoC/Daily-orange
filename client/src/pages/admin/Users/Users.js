7f87591dab0293205f858d2cd3ac5acb305f5029
DonCuervoC <nelson.cuervo89@gmail.com>
2023-05-02 09:44:04 -0400

    FrontEnd: modal to show forms OK

--- client/src/pages/admin/Users/Users.js
+++ client/src/pages/admin/Users/Users.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Users.scss";
 import { Button, Tab } from 'semantic-ui-react';
+import { BasicModal } from "../../../components/Shared";
 
 export function Users() {
+
+  const [showModal,setShowModal] = useState(false);
+
+  const onOpenCloseModal = () => setShowModal((prevState) => !prevState)
 
   const panes = [
     {
@@ -30,11 +35,16 @@ export function Users() {
     <>
       {/* <h1>We are in pages/admin/users/Users.js</h1> */}
       <div className='user-pages'>
-        <Button className='user-pages__add' primary onClick={() => console.log("Open form")}>
+        <Button className='user-pages__add' primary onClick={onOpenCloseModal}>
           New User
         </Button>
         <Tab menu={{ secondary: true }} panes={panes} />
       </div>
+
+      {/* show, close, title, size, children */}
+      <BasicModal show={showModal} close={onOpenCloseModal} title="Create new user"  >
+        <h2>Form to create new users</h2>
+      </BasicModal>
 
     </>
   );
