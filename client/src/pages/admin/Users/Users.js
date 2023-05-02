2370ca0ea062609d572d067a47484a5e95a298f6
DonCuervoC <nelson.cuervo89@gmail.com>
2023-05-02 09:22:53 -0400

    FrontEnd: page structure OK

--- client/src/pages/admin/Users/Users.js
+++ client/src/pages/admin/Users/Users.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import "./Users.scss";
+import { Button, Tab } from 'semantic-ui-react';
 
-export  function Users() {
+export function Users() {
+
+  const panes = [
+    {
+      menuItem: "Active Users",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <h2>Active Users</h2>
+        </Tab.Pane>
+
+      ),
+    },
+    {
+      menuItem: "Inactive Users",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <h2>Inactive Users</h2>
+        </Tab.Pane>
+
+      ),
+    },
+  ];
+
+
   return (
-    <div>
-      <h1>We are in pages/admin/users/Users.js</h1>
-    </div>
+    <>
+      {/* <h1>We are in pages/admin/users/Users.js</h1> */}
+      <div className='user-pages'>
+        <Button className='user-pages__add' primary onClick={() => console.log("Open form")}>
+          New User
+        </Button>
+        <Tab menu={{ secondary: true }} panes={panes} />
+      </div>
+
+    </>
   );
 }
